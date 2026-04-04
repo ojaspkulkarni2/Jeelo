3b03dde4ff54ded712f3cb09f0f2f84c6abc5a54
Swati Kulkarni <ojas.kulkarni646@gmail.com>
2026-04-04 15:27:27 +0530

    bugs

--- Jeelo/vite.config.ts
+++ Jeelo/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
+import path from "path";
 
 export default defineConfig({
   plugins: [
-    cloudflareDevProxy(),
+    cloudflareDevProxy({
+      getLoadContext({ context }) {
+        return { cloudflare: context.cloudflare };
+      },
+    }),
     reactRouter(),
   ],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
 });
